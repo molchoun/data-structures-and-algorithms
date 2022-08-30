af84e91cd21eb8dece1f9b02cdcc6451035cce34
Arsen <102597298+molchoun@users.noreply.github.com>
2022-08-30 15:06:30 +0400

    Implement Stack

---
+++ Stack.py
@@ -0,0 +1,48 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class Stack:
+    def __init__(self):
+        self.top = None
+        self.bottom = None
+        self.len = 0
+
+    def peak(self):
+        return self.top
+
+    def push(self, data):
+        if self.len == 0:
+            self.top = self.bottom = Node(data)
+        else:
+            holding_pointer = self.top
+            self.top = Node(data)
+            self.top.next = holding_pointer
+        self.len += 1
+        return self.top
+
+    def pop(self):
+        if not self.top:
+            return None
+        elif self.len == 1:
+            self.bottom = None
+        current_node = self.top
+        self.top = current_node.next
+        self.len -= 1
+        return current_node
+
+
+
+if __name__ == '__main__':
+    stack1 = Stack()
+    stack1.push('google')
+    stack1.push('udemy')
+    stack1.push('discord')
+    print(stack1.pop())
+    # print(stack1.peak().next.data)
+    print(stack1.pop())
+    print(stack1.pop())
+    print(stack1.bottom)
+    print(stack1.len)
