9176bc60a05d6ace7ce5f8366cb370d239e0a57b
molchoun <movsisian.arsen@gmail.com>
2022-11-21 22:32:53 +0400

    `merge_sort()` implementation

--- Sorting.py
+++ Sorting.py
@@ -47,10 +47,18 @@ def merge_sort(arr):
 
 
 def merge(left, right):
-    if left > right:
-        temp = right
-        left = right
-        right = temp
+    result = []
+    left_idx = 0
+    right_idx = 0
+    while (left_idx < len(left)) and (right_idx < len(right)):
+        if left[left_idx] < right[right_idx]:
+            result.append(left[left_idx])
+            left_idx += 1
+        else:
+            result.append(right[right_idx])
+            right_idx += 1
+    result += left[left_idx:] + right[right_idx:]
+    return result
 
 
 li = merge_sort([7, 2, 1, 5, 9, 12])
